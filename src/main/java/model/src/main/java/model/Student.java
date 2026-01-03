87f0364eafe01326288b58d3c49f9df91c7d8883
seymanue <sirinseyma2002@gmail.com>
2026-01-04 00:17:07 +0300

    Add Student class with properties and methods

---
+++ src/main/java/model/src/main/java/model/Student.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Student {
+    private int id;
+    private String name;
+    private String department;
+
+    public Student() {}
+
+    public Student(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public Student(String name, String department) {
+        this(0, name, department);
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
+
+    @Override
+    public String toString() {
+        return id + " | " + name + " | " + department;
+    }
+}
